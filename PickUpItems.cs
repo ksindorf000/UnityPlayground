2b95c1a1c7cbfd001c632bbf910d2338ada341a4
ksindorf000 <ksindorf000@gmail.com>
2017-02-23 09:48:38 -0500

    All CS script files for Unity Playground Game

---
+++ PickUpItems.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUpItems : MonoBehaviour
+{
+   
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnCollisionEnter (Collision collision)
+    {
+        GameObject player = GameObject.FindWithTag("Player");
+        PlayerController pCntrl = player.GetComponent<PlayerController>();
+
+        if (collision.gameObject.tag == "PickUpItem")
+        {
+            Destroy(collision.gameObject);
+            pCntrl.AddPoint();
+        }
+    }
+
+
+}
---
+++ PlayerController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour {
+
+    int score;
+    public Text ScoreBoard;
+
+    // Use this for initialization
+    void Start () {
+        score = 0;
+        SetScoreBoardText();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    //Add point for putting the item in the goal
+    internal void AddPoint()
+    {
+        score++;
+        SetScoreBoardText();
+    }
+
+    //Set ScoreBoard Text
+    void SetScoreBoardText ()
+    {
+        if (score < 6)
+        {
+            ScoreBoard.text = "Score: " + score.ToString();
+        }
+        else
+        {
+            ScoreBoard.text = "YOU WIN!!!";
+        }
+    }
+}
